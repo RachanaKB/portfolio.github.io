49eba953ce6be8240a122e0253b0347de253bfe7
RACHANA K B <90059328+RachanaKB@users.noreply.github.com>
2021-11-11 16:28:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function revealMessage() {
+    document.getElementById("hidden").style.display = 'block';
+}
+function reveal() {
+    document.getElementById("hide").style.display = 'block';
+}
+function message() {
+    document.getElementById("invisible").style.display = 'block';
+}
+function showMessage() {
+    document.getElementById("close").style.display = 'block';
+}
